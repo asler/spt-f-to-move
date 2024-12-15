0d7be8b81a397fedd9f9c1961ac6a0b20962c148
alimoncul <alimoncul@gmail.com>
2024-12-15 17:39:00 +0300

    init project

---
+++ Mover.cs
@@ -0,0 +1,78 @@
+﻿using BepInEx.Logging;
+using EFT.UI;
+using EFT.InventoryLogic;
+using Comfort.Common;
+using UnityEngine;
+using EFT.Communications;
+using HarmonyLib;
+using EFT;
+
+namespace ftomove
+{
+    public class Mover : MonoBehaviour
+    {
+        private ManualLogSource _logger; 
+
+        public void Initialize(ManualLogSource logger)
+        {
+            _logger = logger;
+        }
+
+        protected void Start()
+        {
+            _logger?.LogInfo("com.alimoncul.ftomove: Mover Start called!");
+        }
+        public static bool InRaid()
+        {
+            var instance = Singleton<AbstractGame>.Instance;
+            return instance != null && instance.InRaid;
+        }
+
+        public static bool InInventoryScreen()
+        {
+            return ItemUiContext.Instance.ContextType == EItemUiContextType.InventoryScreen;
+        }
+
+        protected void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F) && InRaid() && InInventoryScreen())
+            {
+                var itemUiContextInstance = ItemUiContext.Instance;
+                if (itemUiContextInstance == null)
+                {
+                    return;
+                }
+
+                ItemContextAbstractClass itemContextAbstractClass = itemUiContextInstance.ItemContextAbstractClass;
+                if (itemContextAbstractClass == null)
+                {
+                    return;
+                }
+
+                var traderControllerClass = AccessTools.Field(typeof(ItemUiContext), "traderControllerClass").GetValue(itemUiContextInstance) as TraderControllerClass;
+                if (traderControllerClass == null)
+                {
+                    return;
+                }
+
+                Item item = itemContextAbstractClass.Item;
+
+                var findResult = ItemUiContext.Instance.QuickFindAppropriatePlace(itemContextAbstractClass, traderControllerClass, false, true, true);
+                if (findResult.Failed || !traderControllerClass.CanExecute(findResult.Value))
+                {
+                    return;
+                }
+
+                if (findResult.Value is IDestroyResult destroyResult && destroyResult.ItemsDestroyRequired)
+                {
+                    NotificationManagerClass.DisplayWarningNotification(new GClass3726(item, destroyResult.ItemsToDestroy).GetLocalizedDescription(), ENotificationDurationType.Default);
+                    return;
+                }
+
+                traderControllerClass.RunNetworkTransaction(findResult.Value, null);
+
+                Singleton<GUISounds>.Instance.PlayItemSound(item.ItemSound, EInventorySoundType.pickup, false);
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using UnityEngine;
+using BepInEx.Logging;
+
+namespace ftomove
+{
+    [BepInPlugin("com.alimoncul.ftomove", "ftomove", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Logger;
+
+        protected void Awake()
+        {
+            Logger = base.Logger;
+            Logger.LogInfo("com.alimoncul.ftomove loaded");
+
+            var moverObject = new GameObject("Mover");
+            var mover = moverObject.AddComponent<Mover>();
+
+            mover.Initialize(Logger);
+            DontDestroyOnLoad(moverObject);
+        }
+    }
+}
